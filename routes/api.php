229f315f9654fd273cdc9e3258c0c2965349532d
Aminez03 <amine.zaidi03@gmail.com>
2024-09-25 13:35:04 +0100

    backend

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ScategorieController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+// Route::get("/categories",[CategorieController::class,"index"]);// get 
+
+// Route::post("/categories",[CategorieController::class,"store"]);// post 
+// Route::get("/categories/{id}",[CategorieController::class,"show"]);// get by id 
+// Route::put("/categories/{id}",[CategorieController::class,"update"]);// update
+// Route::delete("/categories/{id}",[CategorieController::class,"destroy"]);// delete
+
+Route::middleware('api')->group(function () {
+    Route::resource('categories', CategorieController::class);
+    });
+Route::middleware('api')->group(function () {
+        Route::resource('scategories', ScategorieController::class);
+    });
